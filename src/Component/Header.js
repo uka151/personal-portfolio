9226433f5495225383dc0d9189d6e8c80e57ee36
uka151 <umeshagrahari87@gmail.com>
2020-06-28 22:16:02 +0530

    Header update

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -6,36 +6,36 @@ import { NavItem, Navbar, Nav, NavbarBrand, NavLink } from 'reactstrap';
 class Header extends Component {
     render() {
         return (
-            <Navbar>
-                <div className="container">
-                    <Nav className="nav justify-content-end">
-                        <NavbarBrand>
-                            <h1>Umesh Kr. Agrahari</h1>
-                        </NavbarBrand>
+            <Navbar dark color="secondary">
+                <div className="container m-0">
 
+                    <NavbarBrand>
+                        <h2>Umesh Kr. Agrahari</h2>
+                    </NavbarBrand>
+                    <Nav className="nav justify-content-end">
                         <NavItem>
-                            <NavLink className="nav-link" to='/#'>
+                            <NavLink className="nav-link" to='/About'>
                                 <span className="fa fa-info fa-lg">About</span>
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className="nav-link" to="/#">
-                                <span className="fa fa-">Skills</span>
+                            <NavLink className="nav-link" to="/Skills">
+                                <span className="fa fa-cogs fa-lg">Skills</span>
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className="nav-link" to="/#">
-                                <span className="fa fa-">Education</span>
+                            <NavLink className="nav-link" to="/Education">
+                                <span className="fa fa-list fa-lg">Education</span>
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className="nav-link" to="/#">
-                                <span className="fa fa-">Project</span>
+                            <NavLink className="nav-link" to="/Project">
+                                <span className="fa fa-task fa-lg">Project</span>
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className="nav-link" to="/#">
-                                <span className="fa fa-">Contact</span>
+                            <NavLink className="nav-link" to="/Contact">
+                                <span className="fa fa-address-card fa-lg">Contact</span>
                             </NavLink>
                         </NavItem>
                     </Nav>
