b17d7cc75548c6259233cfac78aec2f642ff36b5
uka151 <umeshagrahari87@gmail.com>
2020-06-28 15:37:07 +0530

    Add Component

---
+++ src/Component/MainComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MainComponent extends Component{
+    render(){
+        return(
+             <div>Hello Umesh </div>
+        );
+    };
+}
+
+export default MainComponent;
